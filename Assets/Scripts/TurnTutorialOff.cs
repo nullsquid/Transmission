c4642261e8e032eafdf6274555eca33669a1c442
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2018-01-28 15:38:31 -0500

    Finished sequencing game

---
+++ Assets/Scripts/TurnTutorialOff.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTutorialOff : MonoBehaviour {
+
+    public GameObject textObj;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            textObj.SetActive(false);
+        }
+	}
+}
